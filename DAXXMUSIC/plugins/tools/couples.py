d181b9a656f6b52b6629e368ace2c678b68074e7
Subham86589 <167736439+Subham86589@users.noreply.github.com>
2024-05-26 17:45:22 +0530

    Update couples.py

--- DAXXMUSIC/plugins/tools/couples.py
+++ DAXXMUSIC/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from DAXXMUSIC.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="𓊈𒆜彡[𝐃𝚊𝚡𝚡 𝐂𝙲 𝐂𝙻𝚄𝙱 ]彡𒆜𓊉",
-            url=f"https://t.me/ALLTYPECC",
+            text="𓊈⛩️ 𝗕𝗜𝗟𝗟𝗔 || 𝗚𝗔𝗡𝗚 々𓊉",
+            url=f"https://t.me/BILLA_GANG_NTWK",
         ),
     ],
 ]
